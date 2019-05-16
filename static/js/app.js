71b3f0922ce446f9f6aeb28d94e873af2493aac8
l-gar <luis.garcia.wi.2793@gmail.com>
2019-05-15 20:23:04 -0500

    Modal added

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,3 @@
-$(document).ready(function(){
-    var empt = document.getElementById('calouput').innerHTML;
-    if (empt == null || empt == "") {}
-    else if (empt == "Error: incorrect input!") {  $("#output2").modal('show');}
-    else {  $("#output").modal('show');} 
+$(window).on('load',function(){
+    $('#myModal').modal('show');
 });
